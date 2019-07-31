e278ab0f794ba68b696746c8b403d7f6b5357faa
unknown <tranducquylhp@gmail.com>
2019-08-01 02:10:48 +0700

    ok

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,30 @@ public class Main {
                 System.out.println(square);
             }
         }
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle) {
+                Circle circle = (Circle) shape;
+                circle.resize(200);
+            } else if (shape instanceof Rectangle && !(shape instanceof Square)) {
+                Rectangle rectangle = (Rectangle) shape;
+                rectangle.resize(300);
+            } else if (shape instanceof Square) {
+                Square square = (Square) shape;
+                square.resize(400);
+            }
+        }
+        System.out.println();
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle) {
+                Circle circle = (Circle) shape;
+                System.out.println(circle);
+            } else if (shape instanceof Rectangle) {
+                Rectangle rectangle = (Rectangle) shape;
+                System.out.println(rectangle);
+            } else if (shape instanceof Square) {
+                Square square = (Square) shape;
+                System.out.println(square);
+            }
+        }
     }
 }
